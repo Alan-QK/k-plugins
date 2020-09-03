63220166fcbd5dbda32b1613e109ac640a296f54
qiuke <keqiu_728@163.com>
2020-09-03 15:56:58 +0800

    init muiltINput

---
+++ packages/muiltInput/webpack.config.js
@@ -0,0 +1,37 @@
+module.exports={
+  entry:path.join(__dirname,'./src/index.tsx'),
+  mode:'production',
+  module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+      ]
+  },
+  resolve: {
+      extensions: [ '.tsx', '.ts', '.js','.jsx' ]
+  },
+  externals: {    //避免打包
+    'react': {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React',
+    },
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugins:[
+    new CleanWebpackPlugin()
+  ],
+  output:{
+      path:path.resolve(__dirname,'dist'),
+      filename:'index.js',
+      library: "ANTD_BASE_MODULE",
+      libraryTarget:'umd',
+      umdNamedDefine: true
+  },
+}
