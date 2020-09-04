66778ecf53be8cc924e7dc43322878e68772d292
qiuke <keqiu_728@163.com>
2020-09-04 09:06:35 +0800

    update

--- packages/nextI18n/src/index.tsx
+++ packages/nextI18n/src/index.tsx
@@ -1,8 +1,7 @@
 // Used in node environment
 const NextI18Next = require('next-i18next').default;
-const path = require('path')
 
-module.exports = new NextI18Next({
+const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'zh',
   otherLanguages: ['zh', 'en'],
   fallbackLng: 'zh',
@@ -10,9 +9,11 @@ module.exports = new NextI18Next({
   saveMissingTo: 'all',
   keySeparator: '.',
   localeSubpaths: {},
-  localePath: path.resolve('./public/static/locales'),
   interpolation: {
     escapeValue: false // react already safes from xss
   },
   serverLanguageDetection: false
 });
+
+export default NextI18NextInstance;
+module.exports = NextI18NextInstance;
