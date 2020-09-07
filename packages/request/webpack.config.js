3c49a53f68d4732513f493c836f774b3b8d016ec
qiuke <keqiu_728@163.com>
2020-09-07 11:48:41 +0800

    update

--- packages/request/webpack.config.js
+++ packages/request/webpack.config.js
@@ -2,41 +2,47 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports={
-  entry:path.join(__dirname,'./src/index.tsx'),
-  mode:'production',
+module.exports = {
+  entry: path.join(__dirname, './src/index.tsx'),
+  mode: 'production',
   module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        },
-      ]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+    ]
   },
   resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   externals: {    //避免打包
     'react': {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'axios': {
+      commonjs: 'axios',
+      commonjs2: 'axios',
+      amd: 'axios',
+      root: 'Axios',
     },
   },
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
-  plugins:[
+  plugins: [
     new CleanWebpackPlugin()
   ],
-  output:{
-      path:path.resolve(__dirname,'dist'),
-      filename:'index.js',
-      library: "ANTD_BASE_MODULE",
-      libraryTarget:'umd',
-      globalObject: 'this',
-      umdNamedDefine: true
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: "ANTD_BASE_MODULE",
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
   },
 }
