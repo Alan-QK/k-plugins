6b9b52ef93357c6f9026044a20441c86b1bc44c6
qiuke <keqiu_728@163.com>
2020-09-07 17:33:36 +0800

    update tip-btn

---
+++ packages/tipBtn/dist/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+interface Props {
+    title?: string;
+    titleExtra?: any;
+    items: Array<any>;
+    children?: any;
+    position?: any;
+    t: any;
+}
+declare const TipBtn: ({ t, title, titleExtra, items, children, position }: Props) => JSX.Element;
+export default TipBtn;
