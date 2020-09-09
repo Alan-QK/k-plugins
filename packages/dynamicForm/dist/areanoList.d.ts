3458c2f1fe9496c060010f59d6f1eddeb22ab6f7
qiuke <keqiu_728@163.com>
2020-09-09 17:52:20 +0800

    update

--- packages/dynamicForm/dist/areanoList.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    country_id: number;
-    country_code: string;
-    country_name_en: string;
-    country_name_cn: string;
-    ab: string;
-}[];
-export default _default;
--- packages/filterGroup/dist/BatchSearchModal.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-declare const BatchSearchModal: ({ t, visible, batchCallbackFn, params, filterItems }: {
-    t: any;
-    visible: any;
-    batchCallbackFn: any;
-    params?: {} | undefined;
-    filterItems: any;
-}) => JSX.Element;
-export default BatchSearchModal;
