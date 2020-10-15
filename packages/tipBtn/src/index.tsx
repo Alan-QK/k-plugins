9ad9756d176f7637bad702a36332def64000956b
qiuke <keqiu_728@163.com>
2020-10-15 11:11:41 +0800

    update style

--- packages/tipBtn/src/index.tsx
+++ packages/tipBtn/src/index.tsx
@@ -39,6 +39,33 @@ const TipBtn = ({ t, title, titleExtra, items, children, position = 'bottom' }: 
       }
     >
       {children}
+
+      <style jsx>{`
+        ul {
+          list-style: none;
+        }
+        .title {
+          display: flex;
+          flex-flow: row wrap;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .fz-12 {
+          font-size: 12px;
+        }
+        .f-b {
+          font-weight: bold;
+        }
+        .c-warn {
+          color: #faad14;
+        }
+        .c-danger {
+          color: #d26363;
+        }
+        .mr-5 {
+          margin-right: 5px;
+        }
+      `}</style>
     </Popover>
   );
 };
