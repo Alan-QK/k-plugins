6a90658f7f43f4cce0e0dc517386e3a30e1d5798
qiuke <keqiu_728@163.com>
2020-11-06 15:56:09 +0800

    updated

--- packages/renderColumnLongTxt/src/index.tsx
+++ packages/renderColumnLongTxt/src/index.tsx
@@ -3,49 +3,49 @@ import { isString } from 'lodash';
 import { Modal } from 'antd';
 
 const style: CSSProperties = {
-  maxWidth: 400,
-  wordBreak: 'break-all',
-  whiteSpace: 'break-spaces'
+    maxWidth: 400,
+    wordBreak: 'break-all',
+    whiteSpace: 'break-spaces'
 }
 
 const fontStyle: CSSProperties = {
-  fontSize: '12px'
+    fontSize: '12px'
 }
 
 export default ({ t, val }) => {
-  const txt = isString(val) ? val : val ? JSON.stringify(val || '') : '';
-  
-  return (
-    <span style={style}>
-      {txt?.length > 75 && (
-        <span>
-          {txt.slice(0, 75).concat('...')}
-          <a
-            style={fontStyle}
-            onClick={() => {
-              Modal.info({
-                title: null,
-                content: (
-                  <span
-                    style={{ ...fontStyle, display: 'inline-block', overflowY: 'auto', maxHeight: 500 }}
-                  >
-                    {txt}
-                  </span>
-                ),
-                width: 760,
-                centered: true,
-                icon: null,
-                okText: t('close'),
-                maskClosable: true,
-              });
-            }}
-          >
-            {t('view-all')}
-          </a>
+    const txt = isString(val) ? val : val ? JSON.stringify(val || '') : '';
+
+    return (
+        <span style={style}>
+            {txt?.length > 75 && (
+                <span>
+                    {txt.slice(0, 75).concat('...')}
+                    <a
+                        style={fontStyle}
+                        onClick={() => {
+                            Modal.info({
+                                title: null,
+                                content: (
+                                    <span
+                                        style={{ ...fontStyle, display: 'inline-block', overflowY: 'auto', maxHeight: 500 }}
+                                    >
+                                        {txt}
+                                    </span>
+                                ),
+                                width: 760,
+                                centered: true,
+                                icon: null,
+                                okText: t('close'),
+                                maskClosable: true,
+                            });
+                        }}
+                    >
+                        {t('common:view-all')}
+                    </a>
+                </span>
+            )}
+            {txt?.length <= 75 && txt}
+            {!txt?.length && '--'}
         </span>
-      )}
-      {txt?.length <= 75 && txt}
-      {!txt?.length && '--'}
-    </span>
-  );
+    );
 }
